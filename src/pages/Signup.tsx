c22c92ba658629d57209359275737b9028174e6f
Ariling <97501226+Ariling@users.noreply.github.com>
2023-08-02 21:13:28 +0900

    커밋 테스트

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      Signup
+      <div>안녕 이거 테스트야!</div>
+    </div>
+  );
 };
 
 export default Signup;
