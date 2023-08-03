90967033a1a817cd0737dd13feab3721bfbce3a7
Ariling <97501226+Ariling@users.noreply.github.com>
2023-08-03 19:39:59 +0900

    [CHORE] 다국어 번역 기능 테스트

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import { useTranslation } from "react-i18next";
+import { useRecoilState } from "recoil";
+import { TranslationAtom } from "../store/atom";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login Gyeongbin</div>;
+  const [currentlang, setLang] = useRecoilState(TranslationAtom);
+  const { t, i18n } = useTranslation();
+  const languageRef = useRef<null | HTMLDivElement>(null);
+
+  useEffect(() => {
+    i18n.changeLanguage(currentlang);
+  }, []);
+
+  const handleChangeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    setLang(i18n.language);
+    console.log(currentlang);
+  };
+
+  return (
+    <div>
+      <header>
+        {t(`header.login`)}
+        <div ref={languageRef}>
+          {t(`header.language`)}
+          <ul>
+            <li onClick={() => handleChangeLanguage("ko")}>한국어</li>
+            <li onClick={() => handleChangeLanguage("en")}>English</li>
+          </ul>
+        </div>
+      </header>
+      <Link to="/signup">회원가입</Link>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import { useEffect, useRef } from "react";
+import { useRecoilState } from "recoil";
+import { TranslationAtom } from "../store/atom";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [currentlang, setLang] = useRecoilState(TranslationAtom);
+  const { t, i18n } = useTranslation();
+  const languageRef = useRef<null | HTMLDivElement>(null);
+  useEffect(() => {
+    i18n.changeLanguage(currentlang);
+    console.log(currentlang);
+  }, []);
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    setLang(i18n.language);
+    console.log(currentlang);
+  };
   return (
     <div>
-      Signup
-      <div>안녕 이거 테스트야!</div>
+      <header>
+        {t(`header.register`)}
+        <div ref={languageRef}>
+          {t(`header.language`)}
+          <ul>
+            <li onClick={() => changeLanguage("ko")}>한국어</li>
+            <li onClick={() => changeLanguage("en")}>English</li>
+          </ul>
+        </div>
+      </header>
+      <Link to="/">로그인</Link>
     </div>
   );
 };
