d7f8621cac1d1267d7991db1df4b1495c4c57a0c
Gyeongbin <tarabin49@gmail.com>
2023-08-04 22:48:02 +0900

    [FEAT]common style 컴포넌트 생성

---
+++ src/common/commonstyle.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 92vw;
+    height: 92vh;
+    border: 1px solid white;
+`
+
+export const Header = styled.div`
+    display: flex;
+    flex-basis: 10%;
+    width: 90%;
+    border: 1px solid white;
+`
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-basis: 88%;
+    width : 90%;
+    border: 1px solid white;
+`
+
+export const Article = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-basis: 50%;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+`
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,37 +1,31 @@
-import { useEffect, useRef } from "react";
-import { useRecoilState } from "recoil";
-import { TranslationAtom } from "../store/atom";
-import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+// import React from 'react'
+import { styled } from 'styled-components'
+import { Article, Container, Header, Wrapper } from '../common/commonstyle'
 
 const Signup = () => {
-  const [currentlang, setLang] = useRecoilState(TranslationAtom);
-  const { t, i18n } = useTranslation();
-  const languageRef = useRef<null | HTMLDivElement>(null);
-  useEffect(() => {
-    i18n.changeLanguage(currentlang);
-    console.log(currentlang);
-  }, []);
-  const changeLanguage = (lang: string) => {
-    i18n.changeLanguage(lang);
-    setLang(i18n.language);
-    console.log(currentlang);
-  };
   return (
-    <div>
-      <header>
-        {t(`header.register`)}
-        <div ref={languageRef}>
-          {t(`header.language`)}
-          <ul>
-            <li onClick={() => changeLanguage("ko")}>한국어</li>
-            <li onClick={() => changeLanguage("en")}>English</li>
-          </ul>
-        </div>
-      </header>
-      <Link to="/">로그인</Link>
-    </div>
-  );
-};
+    <Container>
+      <Header>여기는 헤더</Header>
+      <Wrapper>
+        <Article>여기에 이미지</Article>
+        <Article>
+          <TestInputBox />
+          <TestInputBox />
+          <TestInputBox />
+          <TestInputBox />
+        </Article>
+      </Wrapper>
+    </Container>
+  )
+}
 
-export default Signup;
+export default Signup
+
+// 테스트용 input box
+const TestInputBox = styled.div`
+  display: flex;
+  flex-basis: 10%;
+  width: 80%;
+  background-color: whitesmoke;
+  margin: 5%;
+`
