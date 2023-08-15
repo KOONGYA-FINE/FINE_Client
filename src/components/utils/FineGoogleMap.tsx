8b1d5507afee224f483da0103fe69ac4892c8ea8
Ariling <97501226+Ariling@users.noreply.github.com>
2023-08-15 21:17:25 +0900

    [Feat] 음식점 찾기 눌렀을 시 등록페이지 넘어갈 때 해당 음식점 정보 유지 기능 구현

--- src/components/utils/FineGoogleMap.tsx
+++ src/components/utils/FineGoogleMap.tsx
@@ -1,138 +1,61 @@
-import React, { useState } from "react";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  Marker,
-  StandaloneSearchBox,
-  LoadScript,
-} from "@react-google-maps/api";
+import React, { useMemo, useState } from "react";
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
 import { styled } from "styled-components";
 
-const center = {
-  lat: 37.5649867,
-  lng: 126.985575,
-};
+// const center = {
+//   lat: 37.5649867,
+//   lng: 126.985575,
+// };
+
+const myStyles = [
+  {
+    featureType: "poi",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }],
+  },
+];
 
 const OPTIONS = {
-  minZoom: 4,
-  maxZoom: 18,
+  minZoom: 15,
+  maxZoom: 19,
+  styles: myStyles,
+  streetViewControl: false,
 };
 
-interface Place {
-  position: google.maps.LatLngLiteral;
-  marker?: google.maps.Marker; // Marker 인스턴스를 저장할 속성 추가
-}
-
-function FineGoogleMap() {
+function FineGoogleMap({ lat, lng }: { lat: number; lng: number }) {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_KEY as string,
   });
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
-  const [searchBox, setSearchBox] =
-    useState<google.maps.places.SearchBox | null>(null);
-  const [places, setPlaces] = useState<Place[]>([]);
+  const center = useMemo(() => ({ lat: lat, lng: lng }), []);
 
-  const onLoad = (mapInstance: google.maps.Map) => {
-    setMap(mapInstance);
-
+  const onLoad = React.useCallback(function callback(map: any) {
     const bounds = new window.google.maps.LatLngBounds(center);
-    mapInstance.fitBounds(bounds);
+    map.fitBounds(bounds);
 
-    const inputElement = document.getElementById(
-      "search-box-input"
-    ) as HTMLInputElement;
-    const searchBoxInstance = new window.google.maps.places.SearchBox(
-      inputElement
-    );
-    setSearchBox(searchBoxInstance);
+    setMap(map);
+  }, []);
 
-    searchBoxInstance.addListener("places_changed", () => {
-      const newPlaces = searchBoxInstance.getPlaces();
-
-      if (typeof newPlaces === "undefined" || newPlaces.length === 0) {
-        return;
-      }
-
-      // Clear existing markers
-      places.forEach((place) => {
-        if (place.marker) {
-          place.marker.setMap(null);
-        }
-      });
-
-      // Create new places with markers
-      const updatedPlaces: Place[] = [];
-
-      newPlaces.forEach((newPlace) => {
-        if (newPlace.geometry && newPlace.geometry.location) {
-          const newMarker = new google.maps.Marker({
-            position: newPlace.geometry.location.toJSON(),
-            map: mapInstance,
-          });
-
-          updatedPlaces.push({
-            position: newPlace.geometry.location.toJSON(),
-            marker: newMarker,
-          });
-        }
-      });
-
-      setPlaces(updatedPlaces);
-    });
-  };
-
-  const onUnmount = () => {
+  const onUnmount = React.useCallback(function callback(map: any) {
     setMap(null);
-    setSearchBox(null);
-  };
+  }, []);
 
-  return (
+  return isLoaded ? (
     <Wrapper>
-      <LoadScript
-        googleMapsApiKey={import.meta.env.VITE_GOOGLE_KEY as string}
-        libraries={["places"]}
-        language="en"
+      <GoogleMap
+        mapContainerClassName="map-container"
+        center={center}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={OPTIONS}
       >
-        <GoogleMap
-          mapContainerClassName="map-container"
-          center={center}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-          options={OPTIONS}
-        >
-          {places.map((place, index) => (
-            <Marker key={index} position={place.position} />
-          ))}
-        </GoogleMap>
-        <StandaloneSearchBox
-          onLoad={(searchBoxInstance) => setSearchBox(searchBoxInstance)}
-        >
-          <input
-            id="search-box-input"
-            type="text"
-            placeholder="Search for a place"
-            style={{
-              boxSizing: `border-box`,
-              border: `1px solid transparent`,
-              width: `240px`,
-              height: `32px`,
-              padding: `0 12px`,
-              borderRadius: `3px`,
-              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-              fontSize: `14px`,
-              outline: `none`,
-              textOverflow: `ellipses`,
-              position: "absolute",
-              top: "10px",
-              left: "10px",
-              zIndex: 999,
-            }}
-          />
-        </StandaloneSearchBox>
-      </LoadScript>
+        <MarkerF position={center} />
+      </GoogleMap>
     </Wrapper>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
@@ -141,6 +64,6 @@ export default React.memo(FineGoogleMap);
 const Wrapper = styled.div`
   .map-container {
     width: 80vw;
-    height: 60vh;
+    height: 40vh;
   }
 `;
--- src/pages/food/FoodRegister.tsx
+++ src/pages/food/FoodRegister.tsx
@@ -1,134 +1,34 @@
 import React, { useEffect } from "react";
-
-async function getLatLonFromAddress(address: string) {
-  return new Promise<google.maps.LatLng>((resolve, reject) => {
-    const geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ address }, (results, status) => {
-      if (status === google.maps.GeocoderStatus.OK && results !== null) {
-        console.log(results);
-        const location = results[0].geometry.location;
-        resolve(location);
-      } else {
-        reject(
-          new Error(
-            "Geocode was not successful for the following reason: " + status
-          )
-        );
-      }
-    });
-  });
-}
+import { useRecoilValue } from "recoil";
+import { registerProps } from "../../store/atom";
+import StarEvaluation from "../../components/utils/StarEvaluation";
+import { useWithRoutePageFunc } from "../../hooks/useRoutePageFunc";
+import FineGoogleMap from "../../components/utils/FineGoogleMap";
 
 const FoodRegister: React.FC = () => {
+  const registerProp = useRecoilValue(registerProps);
+  const navigate = useWithRoutePageFunc();
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${
-      import.meta.env.VITE_GOOGLE_KEY as string
-    }&libraries=places&language=en&callback=initAutocomplete`;
-    script.async = true;
-    script.defer = true;
-    document.head.appendChild(script);
-
-    window.initAutocomplete = () => {
-      initMap();
-    };
-  }, []);
-
-  const initMap = () => {
-    const map = new window.google.maps.Map(
-      document.getElementById("map") as HTMLElement,
-      {
-        center: { lat: 37.5649867, lng: 126.985575 },
-        zoom: 13,
-        mapTypeId: "roadmap",
-      }
-    );
-    const input = document.getElementById("pac-input") as HTMLInputElement;
-    const searchBox = new window.google.maps.places.SearchBox(input);
-
-    map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    const infoWindow = new window.google.maps.InfoWindow();
-
-    map.addListener("bounds_changed", () => {
-      searchBox.setBounds(map.getBounds() as google.maps.LatLngBounds);
-    });
-
-    let markers: google.maps.Marker[] = [];
-
-    searchBox.addListener("places_changed", async () => {
-      const places = searchBox.getPlaces();
-
-      if (places.length === 0 || typeof places === "undefined") {
-        return;
-      }
-
-      markers.forEach((marker) => {
-        marker.setMap(null);
-      });
-      markers = [];
-
-      const bounds = new google.maps.LatLngBounds();
-
-      places.forEach(async (place) => {
-        if (!place.geometry || !place.geometry.location) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-
-        const icon = {
-          url: place.icon as string,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25),
-        };
-
-        const marker = new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        });
-
-        marker.addListener("click", async () => {
-          if (place.formatted_address && place.name) {
-            try {
-              const location = await getLatLonFromAddress(
-                place.formatted_address
-              );
-              const { lat, lng } = location;
-              const url = `https://www.google.com/maps/place/${lat},${lng}?hl=en&entry=ttu`;
-              window.open(url, "_blank");
-            } catch (error) {
-              console.error(error);
-            }
-          }
-        });
-
-        markers.push(marker);
-
-        if (place.geometry.viewport) {
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-
-      map.fitBounds(bounds);
-    });
-  };
-
+    if (registerProp.address === "") {
+      alert("먼저 검색을 하고 오십시오");
+      navigate("food/search");
+    }
+    console.log(registerProp);
+  }, [registerProp]);
   return (
-    <div>
-      <input
-        id="pac-input"
-        className="controls"
-        type="text"
-        placeholder="Enter a location"
-      />
-      <div id="map" style={{ height: "50vh", width: "80vw" }}></div>
-    </div>
+    <>
+      <FineGoogleMap lat={registerProp.lat} lng={registerProp.lng} />
+      <div>맛집이름이 무엇인가요?</div>
+      <input value={registerProp.name} disabled />
+      <div>맛집 위치가 어디인지 알려주세요!</div>
+      <input value={registerProp.address} disabled />
+      <div>음식종류를 선택해주세요</div>
+      <div>여기가 들어갈거고..</div>
+      <div>맛집의 별점</div>
+      <StarEvaluation />
+      <div>맛집이 어떤점에서 좋았나요?</div>
+      <textarea />
+    </>
   );
 };
 
